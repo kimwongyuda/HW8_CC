103fbc0bc4cca80df9802ecd92d780dccabd30e5
kimwongyuda <35097915+kimwongyuda@users.noreply.github.com>
2018-05-22 10:40:31 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,18 @@
+
+link_dic= {
+}
+
+file = open('2.5MB.txt','r')
+lines = file.readlines()
+for line in lines:
+    islink = line in link_dic.keys()
+    if islink:
+        link_dic[line] +=1
+    else:
+        link_dic.update({line:1})
+
+# for key in link_dic.keys():
+#     if '\n' in key:
+#         key.replace('\n','')
+for key,value in link_dic.items():
+    print('{} -> {}'.format(key,value))
